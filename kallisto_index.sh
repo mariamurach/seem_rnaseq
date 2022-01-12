e5ac3f1074d0d70f0e3bf744cfed01ef0999ede5
Maria Murach <mm5jy@virginia.edu>
2022-01-12 14:40:06 -0500

    Add protein coding transcript annotation only to be consistent with biopsy files

--- kallisto_index.sh
+++ kallisto_index.sh
@@ -1,1 +1,1 @@
-kallisto index -i /project/tumi/murach/references/kallisto_gencode.v24.idx /project/tumi/murach/references/gencode.v24.transcripts.fa
+kallisto index -i /project/tumi/murach/references/kallisto_gencode.pc.v24.idx /project/tumi/murach/references/gencode.v24.pc_transcripts.fa
